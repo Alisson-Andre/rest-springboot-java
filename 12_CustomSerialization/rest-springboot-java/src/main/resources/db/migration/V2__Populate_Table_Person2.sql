231da4dcf079e077e91ec341b1b58af49544bbed
Alisson-Andre <alissonandresb@gmail.com>
2022-12-15 08:47:41 -0300

    Custom JSON serialization

---
+++ 12_CustomSerialization/rest-springboot-java/src/main/resources/db/migration/V2__Populate_Table_Person2.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'RN', 'Alisson', 'Male', 'Balbino'),
+	(2, 'RN', 'Ivanildo', 'Male', 'Balbino'),
+	(5, 'RN', 'Andre', 'Male', 'Silva'),
+	(6, 'RN', 'joana', 'female', 'Balb'),
+	(8, 'RN', 'joana D', 'female', 'Balbino'),
+	(9, 'RN 2', 'joana 2', 'female', 'Balbino 2');
